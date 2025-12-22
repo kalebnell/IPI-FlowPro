70e13f38a78476f1680f0269e171ea1277605454
Kaleb <kaleb.nell@ipipackers.com>
2025-12-22 10:13:23 -0700

    pushing pyinstaller files

--- request testing.py
+++ request testing.py
@@ -1,22 +1,2 @@
-port1 = None
-port2 = None
-port3=None
-port4=None
-
-
-
-
-def func1(portNum):
-    globals()[f"port{portNum}"] = portNum
-
-def func2():
-    global port1,port2,port3,port4
-    ports = [port1,port2,port3,port4]
-    for port in ports:
-        print(port)
-
-func1(1)
-func1(2)
-func1(3)
-func1(4)
-func2()
+n = "5.0"
+print(float(n))
